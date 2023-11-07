faeb758c201861db130945d70415ff5226481f3d
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-11-08 03:08:57 +0600

    search field added

--- src/components/pages/AllFoodItem.jsx
+++ src/components/pages/AllFoodItem.jsx
@@ -1,10 +1,26 @@
-
 const AllFoodItem = () => {
-    return (
-        <div>
-            Food item comming
+  return (
+    <div>
+      Food item comming
+      <div className="max-w-[1200px] mx-auto mb-8 mt-8">
+        <div className="input-group flex justify-center items-center text-center">
+          <input
+            type="text"
+            placeholder="Search…"
+            className="input input-bordered"
+          />
+          <button className="hover:bg-red-700 btn-square text-white w-[110px] bg-[#FF444A]">
+            SEARCH
+          </button>
         </div>
-    );
+      </div>
+
+
+      <div className="max-w-[1200px] mx-auto">
+        
+      </div>
+    </div>
+  );
 };
 
 export default AllFoodItem;
