8e29598db9c7f0b79b54795a85acdc93c4e37a88
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-11-08 15:46:33 +0600

    to zoom img of food on all food page use framer motion

--- src/components/food/Fooditemscard.jsx
+++ src/components/food/Fooditemscard.jsx
@@ -7,15 +7,15 @@ const Fooditemscard = ({ foods }) => {
   return (
     <div>
       <div className="card card-compact w-96 bg-base-100 shadow-xl">
-        <figure>
-          <img src={foodImg} />
+        <figure className="relative overflow-hidden">
+          <img src={foodImg} className="object-cover transition-transform transform duration-1000 hover:scale-110" />
         </figure>
         <div className="card-body">
-          <h2 className="card-title">{foodName}</h2>
-          <p>{foodCategory}</p>
-          <p>{price}</p>
-          <p>{quantity}</p>
-          <div className="card-actions justify-end">
+          <h2 className="card-title font-extrabold">{foodName}</h2>
+          <p className="bg-gray-100 text-center bg-cover p-4 text-xl font-bold rounded-md">{foodCategory}</p>
+          <p className="font-bold">Price:$ {price}</p>
+          <p>Quantity: {quantity}</p>
+          <div className="card-actions justify-center">
             <Link to={`{/fooditems/${_id}}`}>
               <button className="btn btn-primary">DETAILS</button>
             </Link>
