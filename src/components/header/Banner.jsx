51eedf97ee9e111edc2db6cde1efa041185ee8f7
sabbirsiddique <sabbirsiddique6798@gmail.com>
2023-11-08 21:24:02 +0600

    banner page details added

--- src/components/header/Banner.jsx
+++ src/components/header/Banner.jsx
@@ -9,12 +9,9 @@ const Banner = () => {
         <div className="hero-overlay bg-opacity-60"></div>
         <div className="hero-content text-center text-neutral-content">
           <div className="max-w-md">
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <h1 className="mb-5 text-5xl font-bold">Greetings...</h1>
             <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-              img
+            A restaurant is a commercial establishment that prepares and serves food to paying customers. Restaurants are a popular social hub for dining, and they can range from small, casual establishments to large, fine-dining restaurants
             </p>
             <button className="btn btn-primary">Get Started</button>
           </div>
